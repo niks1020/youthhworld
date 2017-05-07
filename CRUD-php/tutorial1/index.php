ac9020a0efaa8f837beb132eb01c6d7b60caf0ff
Nikhil Pandit <130120107057@git.org.in>
2017-05-07 15:46:47 +0530

    Delete index.php

--- CRUD-php/tutorial1/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("Connection.php");
-if($conn==true) //This line checks weather the connection variable's 
-                //is connected to database or not.
- echo "Connected!";
-else 
- echo "Not Connected!";
-?>
