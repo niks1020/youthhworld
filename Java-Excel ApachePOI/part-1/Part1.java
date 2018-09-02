b28ea51f8a4c20baa20ff9f161ed145f3dd07836
Nikhil Pandit <nikhil.pandit1020@gmail.com>
2018-09-02 15:58:33 +0530

    Add files via upload

---
+++ Java-Excel ApachePOI/part-1/Part1.java
@@ -0,0 +1,77 @@
+package forBlog;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import generic.GeneralCode;
+
+public class Part1 {
+	public static void main(String args[]) throws IOException {
+		
+		
+		byte[] json = Files.readAllBytes(Paths.get("testPart1.json"));
+
+		ObjectMapper mapper = new ObjectMapper();
+		List<TestJson> myObjects = Arrays.asList(mapper.readValue(json, TestJson[].class));
+		
+		String[] myHeaders = {"First Name","Last Name","Occupation","City"};
+		int sheetRowCount = 0;
+		
+		Workbook wb = new XSSFWorkbook();
+		Sheet sheet = wb.createSheet("FirstSheet");
+		Row row = sheet.createRow(sheetRowCount++);
+		Cell cell= null;
+		
+		int cellCounter = 0;
+		for(String key: myHeaders) {
+			cell = row.createCell(cellCounter);
+			cell.setCellValue(myHeaders[cellCounter++]);
+			cell.setCellStyle(GeneralCode.getBorderedCellStyle(wb, BorderStyle.THICK));
+		}
+		cellCounter = 0;
+		
+		int numRows = myObjects.size();
+		
+		for(int i=0; i<numRows;i++) {
+			row = sheet.createRow(sheetRowCount++);
+			TestJson currentObject = myObjects.get(i);
+			cellCounter = 0;
+			for(String key: myHeaders) {
+				cell = row.createCell(cellCounter);
+				cell.setCellValue(currentObject.getData(myHeaders[cellCounter++]));
+				cell.setCellStyle(GeneralCode.getBorderedCellStyleWithBackground(wb, BorderStyle.HAIR, IndexedColors.AQUA.getIndex(), FillPatternType.SOLID_FOREGROUND, IndexedColors.BLACK.getIndex()));
+			}
+		}
+		
+		FileOutputStream fileOut = new FileOutputStream("BlogDemo.xlsx");
+		wb.write(fileOut);
+		fileOut.close();
+	
+		
+		if(wb!=null)
+			wb.close();
+		System.out.println("Done!");
+	}
+}
